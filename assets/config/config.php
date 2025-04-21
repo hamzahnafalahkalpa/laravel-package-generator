755728d0467762832ad4e915414a4a84898ff386
Hamzah <hamzahnafalah@gmail.com>
2025-04-21 16:16:52 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -12,7 +12,7 @@ return [
     ],
     'patterns'        => [
         'repository' => [
-            'published_at' => base_path('Modules'),
+            'published_at' => base_path('repositories'),
             //files has same structure with main generate
             'generates' => [
                 'asset'             => ['type' => 'dir','path' => '../assets', 'generate' => true, 'stub' => null, 'files'=>[]],
@@ -28,7 +28,9 @@ return [
                     'CommandServiceProvider' => ['generate' => true, 'stub' => 'CommandServiceProvider.php.stub'],
                     'RouteServiceProvider'   => ['generate' => true, 'stub' => 'RouteServiceProvider.php.stub']
                 ]],
-                'contract'          => ['type' => 'dir','path' => 'Contracts', 'generate' => true, 'stub' => null, 'files'=>[]],
+                'contract'        => ['type' => 'dir','path' => 'Contracts', 'generate' => true, 'stub' => null, 'files' => [
+                    '{{CLASS_BASENAME}}'  => ['type' => 'file','path' => '', 'generate' => true, 'stub' => 'main-contract.php.stub'],
+                ]],
                 'concern'           => ['type' => 'dir','path' => 'Concerns', 'generate' => true, 'stub' => null, 'files'=>[]],
                 'command'           => ['type' => 'dir','path' => 'Commands', 'generate' => true, 'stub' => null, 'files'=>[
                     'SeedCommand'    => ['generate' => true, 'stub' => 'SeedCommand.php.stub'],
